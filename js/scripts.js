2517b14e2d0d7f15185bb6007ed930229399883f
Tristan Setha and Young Liu <tristan@>
2019-01-30 14:02:19 -0800

    refactor some functions.

--- js/scripts.js
+++ js/scripts.js
@@ -46,224 +46,68 @@ var gameResult = function(){
   else{
     $("#results").text("O has won!");
   }
+  $("#results").append("<br><button class='btn btn-info' id='resetButton'>Play again?</button>");
+  $("#resetButton").click(function(){
+    location.reload();
+  });
+}
+
+var clickEvent = function(boxNumber){
+  if(ticTacToe.myBoard[boxNumber].myValue === "x" || ticTacToe.myBoard[boxNumber].myValue === "o" ){
+    alert("That spot has already been taken!");
+  }
+  else{
+    if(gameState % 2 === 0){
+      ticTacToe.myBoard[boxNumber].myValue = "x";
+      $("#box" + boxNumber + "Content").text("x");
+      $("#results").text("It is O's turn.");
+    }
+    else{
+      ticTacToe.myBoard[boxNumber].myValue = "o";
+      $("#box" + boxNumber + "Content").text("o");
+      $("#results").text("It is X's turn.");
+    }
+    if(ticTacToe.assessWin()){
+      gameResult();
+    }
+    else if(gameState === 8){
+      $("#results").text("The game has ended in a draw.");
+      $("#results").append("<br><button class='btn btn-info' id='resetButton'>Play again?</button>");
+      $("#resetButton").click(function(){
+        location.reload();
+      });
+    }
+    gameState++;
+  }
 }
 
 var attachGameListeners = function(){
   $("#box0").click(function(){
-    if(ticTacToe.myBoard[0].myValue === "x" || ticTacToe.myBoard[0].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[0].myValue = "x";
-        $("#box0Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[0].myValue = "o";
-        $("#box0Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(0);
   });
   $("#box1").click(function(){
-    if(ticTacToe.myBoard[1].myValue === "x" || ticTacToe.myBoard[1].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[1].myValue = "x";
-        $("#box1Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[1].myValue = "o";
-        $("#box1Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(1);
   });
   $("#box2").click(function(){
-    if(ticTacToe.myBoard[2].myValue === "x" || ticTacToe.myBoard[2].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[2].myValue = "x";
-        $("#box2Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[2].myValue = "o";
-        $("#box2Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(2);
   });
   $("#box3").click(function(){
-    if(ticTacToe.myBoard[3].myValue === "x" || ticTacToe.myBoard[3].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[3].myValue = "x";
-        $("#box3Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[3].myValue = "o";
-        $("#box3Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(3);
   });
   $("#box4").click(function(){
-    if(ticTacToe.myBoard[4].myValue === "x" || ticTacToe.myBoard[4].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[4].myValue = "x";
-        $("#box4Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[4].myValue = "o";
-        $("#box4Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(4);
   });
   $("#box5").click(function(){
-    if(ticTacToe.myBoard[5].myValue === "x" || ticTacToe.myBoard[5].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[5].myValue = "x";
-        $("#box5Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[5].myValue = "o";
-        $("#box5Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(5);
   });
   $("#box6").click(function(){
-    if(ticTacToe.myBoard[6].myValue === "x" || ticTacToe.myBoard[6].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[6].myValue = "x";
-        $("#box6Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[6].myValue = "o";
-        $("#box6Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(6);
   });
   $("#box7").click(function(){
-    if(ticTacToe.myBoard[6].myValue === "x" || ticTacToe.myBoard[6].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[7].myValue = "x";
-        $("#box7Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[7].myValue = "o";
-        $("#box7Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(7);
   });
   $("#box8").click(function(){
-    if(ticTacToe.myBoard[6].myValue === "x" || ticTacToe.myBoard[6].myValue === "o" ){
-      alert("That spot has already been taken!");
-    }
-    else{
-      if(gameState % 2 === 0){
-        ticTacToe.myBoard[8].myValue = "x";
-        $("#box8Content").text("x");
-        $("#results").text("It is O's turn.");
-      }
-      else{
-        ticTacToe.myBoard[8].myValue = "o";
-        $("#box8Content").text("o");
-        $("#results").text("It is X's turn.");
-      }
-      if(ticTacToe.assessWin()){
-        gameResult();
-      }
-      else if(gameState === 8){
-        $("#results").text("The game has ended in a draw.");
-      }
-      gameState++;
-    }
+    clickEvent(8);
   });
 }
 
